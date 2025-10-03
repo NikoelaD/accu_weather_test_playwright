5090a1403a65be7d7e28a71b9d4b009d6481d563
NikiAmusnet <nikathekilla2017@gmail.com>
2025-10-03 13:54:34 +0300

    remove loc

--- src/config/locators.ts
+++
@@ -1,4 +0,0 @@
-export const WeatherElements = {
-    web:{webCityLoc:"//input[contains(@class,'search-input')]"},
-    mobile:{mobCityLoc:"(//input[contains(@placeholder,'Search your Address')])[2]"}
-}
